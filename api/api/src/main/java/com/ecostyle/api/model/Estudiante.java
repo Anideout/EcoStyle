3c8ba3427b0a3ec4a5f525425c8a497311f9db80
anideout <m.borquez.n.3i@gmail.com>
2025-05-20 11:03:36 +0200

    nuevo cambio

--- api/api/src/main/java/com/ecostyle/api/model/Estudiante.java
+++ api/api/src/main/java/com/ecostyle/api/model/Estudiante.java
@@ -14,80 +14,49 @@ public class Estudiante {
 
     public Estudiante() {
         this.rut = "";
-        this.contrasena ="";
+        this.contrasena = "";
         this.correo = "";
         this.nombre = "";
         this.apellido = "";
 
     }
-    
 
-    /**
-     * @return String return the rut
-     */
     public String getRut() {
         return rut;
     }
 
-    /**
-     * @param rut the rut to set
-     */
     public void setRut(String rut) {
         this.rut = rut;
     }
 
-    /**
-     * @return String return the contrasena
-     */
     public String getContrasena() {
         return contrasena;
     }
 
-    /**
-     * @param contrasena the contrasena to set
-     */
     public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
 
-    /**
-     * @return String return the correo
-     */
     public String getCorreo() {
         return correo;
     }
 
-    /**
-     * @param correo the correo to set
-     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
-    /**
-     * @return String return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return String return the apellido
-     */
     public String getApellido() {
         return apellido;
     }
 
-    /**
-     * @param apellido the apellido to set
-     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
